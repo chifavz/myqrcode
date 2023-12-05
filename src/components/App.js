fd27a652a13eb9fe41cba8e8b497d61e600b6b88
chifavz <chifavz@gmail.com>
2023-12-05 12:21:13 -0500

    added image

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="container">
+      <img src='/qrcode.png' alt='qrcode' />
+      <h1>Qr code project</h1>
+    </div>
+  );
+}
+
+export default App;
+
